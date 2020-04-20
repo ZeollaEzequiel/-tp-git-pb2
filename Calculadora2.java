066dfb8e81be2d04f1b1694a65bb1bb9aa47489e
Ezequiel-git <63479895+Ezequiel-gif@users.noreply.github.com>
2020-04-20 18:58:11 -0300

    punto 8, cambio de mejora en el archivo

--- Calculadora2.java
+++ Calculadora2.java
@@ -8,15 +8,18 @@ public class Ahorcado {
 		Integer b;
 		Integer opcion;
 		Integer resultado;
+
 		do{
 		System.out.println("Ingrese su operación /n 3 para sumar /n 2 para
-restar /n 1 para multiplicar /n 4 para dividir");
+		restar /n 1 para multiplicar /n 4 para dividir");
+
 		opcion = teclado.next();
 		}while(opcion < 1 && opcion > 4)
 
 
 		System.out.println("Ingrese el primer numero");
 		a = teclado.next();
+
 		System.out.println("Ingrese el segundo numero");
 		a = teclado.next();
 
